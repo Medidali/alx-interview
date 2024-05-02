b1d8c09ef85e49bac6a0f860d3a131739aef0fde
Medidali <m.mohamedidali@gmail.com>
2024-05-02 10:36:37 +0100

    tasks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,50 @@
 #!/usr/bin/python3
-"""Script that reads stdin line by line and computes metrics"""
+"""
+log parsing
+"""
+
 import sys
+import re
 
 
-def print_metrics(status_codes, file_size):
-    """function to print status codes and total file size"""
-    print("File size: {}".format(file_size))
-    for k, v in sorted(status_codes.items()):
-        if v > 0:
-            print("{}: {}".format(k, v))
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0,
-}
-counter = 1
-file_size = 0
-try:
-    for line in sys.stdin:
-        splited_line = line.strip().split()
-        if len(splited_line) < 7:
-            continue
-        file_size += int(splited_line[-1])
-        status_code = splited_line[-2]
-        if status_code in status_codes:
-            status_codes[status_code] += 1
-        if counter == 10:
-            print_metrics(status_codes, file_size)
-            counter = 1
-        else:
-            counter += 1
-    print_metrics(status_codes, file_size)
-except KeyboardInterrupt:
-    print_metrics(status_codes, file_size)
-    raise
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
+
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
+
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
+
+                # File size
+                log["file_size"] += file_size
+
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
+
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+        output(log)
