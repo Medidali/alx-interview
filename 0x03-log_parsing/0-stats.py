fff5374f227e41e39c6ff91d59babfe1ac0b9787
Medidali <m.mohamedidali@gmail.com>
2024-05-02 11:04:34 +0100

    tasks

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,12 +8,12 @@ import sys
 if __name__ == '__main__':
 
     filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    codes = {"200", "301", "400", "401", "403", "404", "405", "500"}
     stats = {k: 0 for k in codes}
 
     def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
+        print("File size: {:d}".format(file_size))
+        for k, v in sorted(stats.items(), key=lambda x: int(x[0])):
             if v:
                 print("{}: {}".format(k, v))
 
@@ -21,16 +21,12 @@ if __name__ == '__main__':
         for line in sys.stdin:
             count += 1
             data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
+            if len(data) >= 9 and data[-2] in codes:
+                try:
+                    filesize += int(data[-1])
+                    stats[data[-2]] += 1
+                except ValueError:
+                    pass
             if count % 10 == 0:
                 print_stats(stats, filesize)
         print_stats(stats, filesize)
