37b01f9ded4624460c3be38f2aa45622293c6ee9
Medidali <m.mohamedidali@gmail.com>
2024-05-17 21:39:09 +0100

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,56 +2,54 @@
 
 import sys
 
+def solve(n):
+    """Solves the N-Queens problem and returns all possible solutions."""
+    solutions = [[]]
+    for row in range(n):
+        solutions = place_queen(row, n, solutions)
+    return solutions
 
-def solve(row, column):
-    solver = [[]]
-    for q in range(row):
-        solver = place_queen(q, column, solver)
-    return solver
+def place_queen(row, n, previous_solutions):
+    """Places a queen in a given row and generates new solutions."""
+    new_solutions = []
+    for solution in previous_solutions:
+        for col in range(n):
+            if is_safe(row, col, solution):
+                new_solutions.append(solution + [col])
+    return new_solutions
 
-
-def place_queen(q, column, prev_solver):
-    solver_queen = []
-    for array in prev_solver:
-        for x in range(column):
-            if is_safe(q, x, array):
-                solver_queen.append(array + [x])
-    return solver_queen
-
-
-def is_safe(q, x, array):
-    if x in array:
-        return (False)
-    else:
-        return all(abs(array[column] - x) != q - column
-                   for column in range(q))
-
+def is_safe(row, col, solution):
+    """Checks if a queen can be placed at (row, col) without being attacked."""
+    for r in range(row):
+        if solution[r] == col or abs(solution[r] - col) == row - r:
+            return False
+    return True
 
 def init():
+    """Initializes the program by parsing the command line arguments."""
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+    
     if sys.argv[1].isdigit():
-        the_queen = int(sys.argv[1])
+        n = int(sys.argv[1])
     else:
         print("N must be a number")
         sys.exit(1)
-    if the_queen < 4:
+    
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-    return(the_queen)
-
+    
+    return n
 
 def n_queens():
-
-    the_queen = init()
-    solver = solve(the_queen, the_queen)
-    for array in solver:
-        clean = []
-        for q, x in enumerate(array):
-            clean.append([q, x])
-        print(clean)
-
+    """Main function to solve the N-Queens problem and print solutions."""
+    n = init()
+    solutions = solve(n)
+    for solution in solutions:
+        print([[row, col] for row, col in enumerate(solution)])
 
 if __name__ == '__main__':
     n_queens()
+
